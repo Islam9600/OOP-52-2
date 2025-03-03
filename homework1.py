71ba302a284a87c7fd85fb7683f19e7e5aa328ac
Islam9600 <islamfufuza073@gmail.com>
2025-03-03 19:32:09 +0600

    HW: homework1 created a hero with name, lvl, hp

---
+++ homework1.py
@@ -0,0 +1,22 @@
+class Hero:
+    def __init__(self, name, lvl, hp):
+        self.name = name
+        self.lvl = lvl
+        self.hp = hp
+
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, мой lvl {self.lvl}, мое HP {self.hp}")
+
+    def is_adult(self):
+        return self.lvl >= 10
+
+hero1 = Hero("Тигрил", 5, 100)
+hero2 = Hero("Ланс", 12, 150)
+hero3 = Hero("Клинт", 8, 80)
+hero4 = Hero("Минотавр", 8, 180 )
+
+# Вызов метода is_adult для каждого героя
+print(hero1.is_adult())  # False
+print(hero2.is_adult())  # True
+print(hero3.is_adult())  # False
+print(hero4.is_adult())  #false
