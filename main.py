b537826dc503f15c8fe78b72bdd134679b9c9d15
Islam9600 <islamfufuza073@gmail.com>
2025-03-10 15:03:15 +0600

    HW: Did hw3

---
+++ main.py
@@ -0,0 +1,10 @@
+from hw3 import Jester  # Импортируем класс Jester из hw3.py
+
+# Создаем экземпляр класса Jester
+jester = Jester(name="Jester Hero")
+
+# Вызовем методы класса Hero через объект jester
+jester.unique_attack()   # Уникальная атака
+jester.unique_scream()   # Уникальный крик
+jester.action()          # В зависимости от случайного числа будет вызван attack, protection или rest
+
